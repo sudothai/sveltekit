764ad4d32ddbbd100fb453fba33cd045f69666a4
Thai Radin <thairadin@outlook.com>
2024-02-12 16:09:54 -0500

    added slug with error code

--- src/routes/posts/create/+page.ts
+++ src/routes/posts/create/+page.ts
@@ -1,5 +1,8 @@
+import { error } from '@sveltejs/kit';
 import type { PageLoad } from './$types';
 
 export const load: PageLoad = async () => {
-	throw new Error('Yikes!');
+	// throw new Error('Yikes!');
+
+	throw error(404, { message: 'Yikes! 😱' });
 };
