ed26cbeda7b988228c2f225ba8bc45103ba75fbd
Thai Radin <thairadin@outlook.com>
2024-02-12 23:02:46 -0500

    created api and called it!

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,18 @@
-<h1>Home Page</h1>
+<script lang="ts">
+    async function subscribe(event: Event) {
+        const form = event.target as HTMLFormElement
+        const data = new FormData(form)
+
+        await fetch('/api/newsletter', {
+            method: 'POST',
+            body: data
+        })
+    }
+</script>
+
+<h1>Newsletter</h1>
+
+<form on:submit|preventDefault={subscribe}>
+    <input type="email" name="email">
+    <button>Subscribe</button>
+</form>
