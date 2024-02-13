acbe8ad8ead24d6e517f9b2a4a78af063839c55a
Thai Radin <thairadin@outlook.com>
2024-02-12 23:16:34 -0500

    pick random out of 30 data to return back

--- src/routes/api/posts/+server.ts
+++ src/routes/api/posts/+server.ts
@@ -6,6 +6,12 @@ import db from '$lib/database';
 export const GET: RequestHandler = async (event) => {
 	console.log(event);
 
-	const posts = await db.post.findMany();
+	const posts = await db.post.findMany({
+		take: Math.round(Math.random() * 30)
+	});
+
+	event.setHeaders({
+		'Cache-Control': 'max-age=60'
+	});
 	return json(posts);
 };
